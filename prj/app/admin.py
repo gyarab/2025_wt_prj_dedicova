4f2ab909d010e86574e81edef541d64818c10e87
atwx123 <wanda.123@seznam.cz>
2026-03-13 12:28:20 +0100

    .

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Meme, user
 
 # Register your models here.
+
+@admin.register(Meme)
+class MemeAdmin(admin.ModelAdmin):
+    list_display=['title']
+@admin.register(user)
+class userAdmin(admin.ModelAdmin):
+    pass
--- prj/app/models.py
+++ prj/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Meme(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(null=True, blank=True)
+    user = models.ForeignKey('user', null=True, on_delete=models.SET_NULL)
+class user(models.Model):
+    username = models.CharField
